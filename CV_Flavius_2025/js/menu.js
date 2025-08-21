aa8cf5eb77eacb936e3e3fbba100aabc5745248e
Flavius Dorel Abutnariti <flapfla89@gmail.com>
2025-08-21 17:46:27 +0200

    Eliminar carpeta antigua CV_Flavius_2025

--- CV_Flavius_2025/js/menu.js
+++
@@ -1,5 +0,0 @@
-// Función para alternar la visibilidad del menú lateral en móvil
-function toggleMenu() {
-    const barra = document.querySelector('.barra-lateral');
-    barra.classList.toggle('visible');
-}
